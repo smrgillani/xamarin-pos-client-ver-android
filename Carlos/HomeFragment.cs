f7c2d48e1ab9b5d242d92b609c2ee4b413154779
syedmusarazagillani <smr.gillani@yahoo.com>
2020-10-10 22:39:29 +0500

    final commit

---
+++ Carlos/HomeFragment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Carlos
+{
+    public class HomeFragment : Android.Support.V4.App.Fragment
+    {
+        
+        public HomeFragment()
+        {
+            
+        }
+        public static Android.Support.V4.App.Fragment newInstance(Context context)
+        {
+            HomeFragment busrouteFragment = new HomeFragment();
+
+            return busrouteFragment;
+        }
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+
+        }
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            ViewGroup root = (ViewGroup)inflater.Inflate(Resource.Layout.activity_rest, null);
+            ListView myList;
+            myList = (ListView)root.FindViewById(Resource.Id.ReslistView);
+            myList.Adapter = new MyCustomListAdapter(RestData.Rests);
+            //Button LoginBtn = (Button)root.FindViewById(Resource.Id.dummybutton_);
+            //LoginBtn.Click += LoginBtn_Click;
+            return root;
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            Toast.MakeText(Application.Context, "Got Hit", ToastLength.Short).Show();
+        }
+    }
+}
---
+++ Carlos/obj/Release/90/android/src/md5499a6e3b6edc0cc5af5780076c2e1d37/ViewHolder.java
@@ -0,0 +1,38 @@
+package md5499a6e3b6edc0cc5af5780076c2e1d37;
+
+
+public class ViewHolder
+	extends java.lang.Object
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("Carlos.ViewHolder, Carlos", ViewHolder.class, __md_methods);
+	}
+
+
+	public ViewHolder ()
+	{
+		super ();
+		if (getClass () == ViewHolder.class)
+			mono.android.TypeManager.Activate ("Carlos.ViewHolder, Carlos", "", this, new java.lang.Object[] {  });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
